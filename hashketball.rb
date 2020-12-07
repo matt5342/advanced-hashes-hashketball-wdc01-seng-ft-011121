68e6f52bd7da142b01f691258dbe9afd8aacc9d6
Matthew Sewell <mattfsewell@gmail.com>
2020-12-07 21:28:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -205,8 +205,7 @@ def big_shoe_rebounds
           size = stats.fetch(:shoe)
        
         end
-        
+                 return stats.fetch(:rebounds)
     end
-         return stats.fetch(:rebounds)
   end
 end
